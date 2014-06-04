873bffe64fe0525ec97814d291e22a83598f1c84
Eugene Chipachenko <eugene.chipachenko@themidnightcoders.com>
2014-06-04 09:24:07 +0300

    Calls from custom code must have BusinessLogic device type

--- src/com/backendless/HeadersManager.java
+++ src/com/backendless/HeadersManager.java
@@ -18,6 +18,7 @@
 
 package com.backendless;
 
+import com.backendless.commons.DeviceType;
 import com.backendless.exceptions.BackendlessException;
 import com.backendless.exceptions.ExceptionMessage;
 
@@ -49,7 +50,12 @@ public class HeadersManager
           instance = new HeadersManager();
           instance.addHeader( HeadersEnum.APP_ID_NAME, Backendless.getApplicationId() );
           instance.addHeader( HeadersEnum.SECRET_KEY_NAME, Backendless.getSecretKey() );
-          instance.addHeader( HeadersEnum.APP_TYPE_NAME, "ANDROID" );
+
+          if (Thread.currentThread().getThreadGroup().getName().equals( "CodeRunner secure group" ))
+            instance.addHeader( HeadersEnum.APP_TYPE_NAME, DeviceType.BL.name() );
+          else
+            instance.addHeader( HeadersEnum.APP_TYPE_NAME, DeviceType.ANDROID.name() );
+
           instance.addHeader( HeadersEnum.API_VERSION, "1.0" );
           instance.addHeaders( Backendless.getHeaders() );
         }
