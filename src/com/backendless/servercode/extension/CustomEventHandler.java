13b3a115273ee1a88632455f5854f80ef6bf5da8
Eugene Chipachenko <eugene.chipachenko@themidnightcoders.com>
2014-06-24 16:07:17 +0300

    BKNDLSS-5813 Add support for security annotations

---
+++ src/com/backendless/servercode/extension/CustomEventHandler.java
@@ -0,0 +1,18 @@
+package com.backendless.servercode.extension;
+
+import javax.naming.event.EventContext;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Eugene Chipachenko
+ * Date: 24.06.14
+ * Time: 16:03
+ */
+public abstract class CustomEventHandler
+{
+  public Map handleEvent( EventContext context, Map ecentArgs )
+  {
+    return null;
+  }
+}
