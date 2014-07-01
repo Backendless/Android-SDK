a4a4feeda4b1aea995822bb0e29fabd874cdb4fb
Sergey Chupov <sergey.chupov@themidnightcoders.com>
2014-07-01 14:11:10 +0300

    bug fixed

--- src/com/backendless/Backendless.java
+++ src/com/backendless/Backendless.java
@@ -41,8 +41,8 @@ public final class Backendless
   public static final Messaging Messaging = com.backendless.Messaging.getInstance();
   public static final Geo Geo = com.backendless.Geo.getInstance();
   public static final Files Files = com.backendless.Files.getInstance();
-  public static final Events Events = com.backendless.Events.getInstance();
   public static final Commerce Commerce = com.backendless.Commerce.getInstance();
+  public static final Events Events = com.backendless.Events.getInstance();
   private static String url = "https://api.backendless.com";
   private static IBackendlessService backendlessService;
   private static IBackendlessService.Init backendlessInitService;
