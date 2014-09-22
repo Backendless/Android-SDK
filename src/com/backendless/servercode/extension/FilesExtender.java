5c4c44bf0a3d02286ac1500e152b927850004cd2
denis.zabolotny <denis.zabolotny@themidnightcoders.com>
2014-09-22 14:27:15 +0300

    add 2 methods in FileService handle (T-6741)

--- src/com/backendless/servercode/extension/FilesExtender.java
+++ src/com/backendless/servercode/extension/FilesExtender.java
@@ -1,3 +1,21 @@
+/*
+ * ********************************************************************************************************************
+ *  <p/>
+ *  BACKENDLESS.COM CONFIDENTIAL
+ *  <p/>
+ *  ********************************************************************************************************************
+ *  <p/>
+ *  Copyright 2012 BACKENDLESS.COM. All Rights Reserved.
+ *  <p/>
+ *  NOTICE: All information contained herein is, and remains the property of Backendless.com and its suppliers,
+ *  if any. The intellectual and technical concepts contained herein are proprietary to Backendless.com and its
+ *  suppliers and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret
+ *  or copyright law. Dissemination of this information or reproduction of this material is strictly forbidden
+ *  unless prior written permission is obtained from Backendless.com.
+ *  <p/>
+ *  ********************************************************************************************************************
+ */
+
 package com.backendless.servercode.extension;
 
 import com.backendless.servercode.ExecutionResult;
@@ -29,6 +47,14 @@ public abstract class FilesExtender
   }
 
   public void afterDeleteFileOrDirectory( RunnerContext context, String fileUrlLocation,
+                                          ExecutionResult result ) throws Exception
+  {
+  }
+  public void beforeSaveFileFromByteArray( RunnerContext context, String fileUrlLocation, Boolean overwrite ) throws Exception
+  {
+  }
+
+  public void afterSaveFileFromByteArray( RunnerContext context, String fileUrlLocation, Boolean overwrite,
                                           ExecutionResult result ) throws Exception
   {
   }
