1e4f24ab76f08626a5d1a71d4ea6330ab6ae1285
scadge <scadgek@live.com>
2014-11-16 15:06:38 +0200

    http://bugs.backendless.com/browse/BKNDLSS-7281

--- src/com/backendless/FootprintsManager.java
+++ src/com/backendless/FootprintsManager.java
@@ -1,10 +1,14 @@
 package com.backendless;
 
+import com.backendless.exceptions.BackendlessException;
 import weborb.reader.AnonymousObject;
 import weborb.reader.ArrayType;
 import weborb.reader.NamedObject;
 
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
 /**
@@ -15,7 +19,7 @@ import java.util.*;
 public class FootprintsManager
 {
   private static final FootprintsManager instance = new FootprintsManager();
-  private static Set<Object> toBeCached = new HashSet<Object>(); //for cyclic entities
+  private static Set<Object> marked = new HashSet<Object>(); //for cyclic entities
   public final Inner Inner = new Inner();
   private final Map<Object, Footprint> persistenceCache = new WeakHashMap<Object, Footprint>();
 
@@ -69,6 +73,11 @@ public class FootprintsManager
   {
     void putMissingPropsToEntityMap( Object entity, Map entityMap )
     {
+      if( marked.contains( entity ) )
+        return;
+      else
+        marked.add( entity );
+
       if( !entityMap.containsKey( Footprint.OBJECT_ID_FIELD_NAME ) )
       {
         String objectId = getObjectId( entity );
@@ -89,6 +98,27 @@ public class FootprintsManager
       {
         Object value = entityMap.get( key );
 
+        if( value instanceof Map )
+        {
+          try
+          {
+            Object entityField = new PropertyDescriptor( (String) key, entity.getClass() ).getReadMethod().invoke( entity );
+            putMissingPropsToEntityMap( entityField, (Map) value );
+          }
+          catch( IllegalAccessException e )
+          {
+            throw new BackendlessException( e );
+          }
+          catch( InvocationTargetException e )
+          {
+            throw new BackendlessException( e );
+          }
+          catch( IntrospectionException e )
+          {
+            throw new BackendlessException( e );
+          }
+        }
+
         if( value instanceof Collection )
         {
           Collection coll = (Collection) value;
@@ -104,27 +134,130 @@ public class FootprintsManager
           }
         }
       }
+
+      marked.remove( entity );
     }
 
-    void duplicateFootprintForObject( Object newEntity, Object oldEntity )
+    void duplicateFootprintForObject( Map serialized, Object newEntity, Object oldEntity )
     {
+      if( marked.contains( newEntity ) )
+        return;
+      else
+        marked.add( newEntity );
+
+      Set<Map.Entry> entries = serialized.entrySet();
+      for( Map.Entry entry : entries )
+      {
+        if( entry.getValue() instanceof Map )
+        {
+          try
+          {
+            Object newEntityField = new PropertyDescriptor( (String) entry.getKey(), newEntity.getClass() ).getReadMethod().invoke( newEntity );
+            Object oldEntityField = new PropertyDescriptor( (String) entry.getKey(), oldEntity.getClass() ).getReadMethod().invoke( oldEntity );
+
+            duplicateFootprintForObject( (Map) entry.getValue(), newEntityField, oldEntityField );
+          }
+          catch( IllegalAccessException e )
+          {
+            throw new BackendlessException( e );
+          }
+          catch( InvocationTargetException e )
+          {
+            throw new BackendlessException( e );
+          }
+          catch( IntrospectionException e )
+          {
+            throw new BackendlessException( e );
+          }
+        }
+      }
+
       Footprint footprint = persistenceCache.get( oldEntity );
 
       if( footprint != null )
         persistenceCache.put( newEntity, footprint );
+
+      marked.remove( newEntity );
     }
 
-    void updateFootprintForObject( Object newEntity, Object oldEntity )
+    void updateFootprintForObject( Map serialized, Object newEntity, Object oldEntity )
     {
+      if( marked.contains( newEntity ) )
+        return;
+      else
+        marked.add( newEntity );
+
+      Set<Map.Entry> entries = serialized.entrySet();
+      for( Map.Entry entry : entries )
+      {
+        if( entry.getValue() instanceof Map )
+        {
+          try
+          {
+            Object newEntityField = new PropertyDescriptor( (String) entry.getKey(), newEntity.getClass() ).getReadMethod().invoke( newEntity );
+            Object oldEntityField = new PropertyDescriptor( (String) entry.getKey(), oldEntity.getClass() ).getReadMethod().invoke( oldEntity );
+
+            updateFootprintForObject( (Map) entry.getValue(), newEntityField, oldEntityField );
+          }
+          catch( IllegalAccessException e )
+          {
+            throw new BackendlessException( e );
+          }
+          catch( InvocationTargetException e )
+          {
+            throw new BackendlessException( e );
+          }
+          catch( IntrospectionException e )
+          {
+            throw new BackendlessException( e );
+          }
+        }
+      }
+
       Footprint footprint = persistenceCache.get( newEntity );
 
       persistenceCache.put( oldEntity, footprint );
-      removeFootprintForObject( newEntity );
+      removeFootprintForObject( serialized, newEntity );
+
+      marked.remove( newEntity );
     }
 
-    void removeFootprintForObject( Object entity )
+    void removeFootprintForObject( Map serialized, Object entity )
     {
+      if( marked.contains( entity ) )
+        return;
+      else
+        marked.add( entity );
+
+      Set<Map.Entry> entries = serialized.entrySet();
+      for( Map.Entry entry : entries )
+      {
+        if( entry.getValue() instanceof Map )
+        {
+          try
+          {
+            Object entityField = new PropertyDescriptor( (String) entry.getKey(), entity.getClass() ).getReadMethod().invoke( entity );
+
+            removeFootprintForObject( (Map) entry.getValue(), entity );
+          }
+          catch( IllegalAccessException e )
+          {
+            throw new BackendlessException( e );
+          }
+          catch( InvocationTargetException e )
+          {
+            throw new BackendlessException( e );
+          }
+          catch( IntrospectionException e )
+          {
+            throw new BackendlessException( e );
+          }
+        }
+      }
+
       persistenceCache.remove( entity );
+
+      marked.remove( entity );
     }
 
     /**
@@ -138,7 +271,11 @@ public class FootprintsManager
      */
     public void putEntityFootprintToCache( Object instance, Object entity )
     {
-      toBeCached.add( entity );
+      if( marked.contains( entity ) )
+        return;
+      else
+        marked.add( entity );
+
       try
       {
         if( instance instanceof BackendlessCollection )
@@ -146,18 +283,15 @@ public class FootprintsManager
           AnonymousObject typedObject = (AnonymousObject) ((NamedObject) entity).getTypedObject();
           ArrayType dataArray = (ArrayType) typedObject.getProperties().get( "data" );
           Object[] instances = ((BackendlessCollection) instance).getCurrentPage().toArray();
-          if( !toBeCached.contains( dataArray ) )
-            putEntityFootprintToCache( instances, dataArray );
+          putEntityFootprintToCache( instances, dataArray );
         }
         else if( entity instanceof NamedObject )
         {
-          if( !toBeCached.contains( ((NamedObject) entity).getTypedObject() ) )
-            putEntityFootprintToCache( instance, ((NamedObject) entity).getTypedObject() );
+          putEntityFootprintToCache( instance, ((NamedObject) entity).getTypedObject() );
         }
         else if( entity instanceof AnonymousObject )
         {
-          if( !toBeCached.contains( ((AnonymousObject) entity).getProperties() ) )
-            putEntityFootprintToCache( instance, ((AnonymousObject) entity).getProperties() );
+          putEntityFootprintToCache( instance, ((AnonymousObject) entity).getProperties() );
         }
         else if( entity instanceof ArrayType )
         {
@@ -165,8 +299,7 @@ public class FootprintsManager
           Object[] arrayInstance = instance instanceof List ? ((List) instance).toArray() : (Object[]) instance;
 
           for( int i = 0; i < arrayInstance.length; i++ )
-            if( !toBeCached.contains( entities[ i ] ) )
-              putEntityFootprintToCache( arrayInstance[ i ], entities[ i ] );
+            putEntityFootprintToCache( arrayInstance[ i ], entities[ i ] );
         }
         else
         {
@@ -178,19 +311,19 @@ public class FootprintsManager
             if( entityEntryValue instanceof NamedObject || entityEntryValue instanceof ArrayType )
             {
               Object innerInstance = getObjectFieldByName( instance, entityEntry.getKey() );
-              if( !toBeCached.contains( entityEntry.getValue() ) )
-                putEntityFootprintToCache( innerInstance, entityEntry.getValue() );
+              putEntityFootprintToCache( innerInstance, entityEntry.getValue() );
             }
           }
 
           Map<String, Object> cachedEntity = (Map<String, Object>) entity;
           persistenceCache.put( instance, Footprint.initFromEntity( cachedEntity ) );
-          toBeCached.remove( cachedEntity );
         }
       }
       catch( Exception e )
       {/*Error in caching process should not fail application*/
       }
+
+      marked.remove( entity );
     }
 
     private Object getObjectFieldByName( Object instance,
