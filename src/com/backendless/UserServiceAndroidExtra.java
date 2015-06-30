8cf0070cf994a475a56325525e68aefcff4c36f3
Julia Valchuk <julia.valchuk@themidnightcoders.com>
2015-06-30 17:58:56 +0300

    BKNDLSS-9526: added easy google login

--- src/com/backendless/UserServiceAndroidExtra.java
+++ src/com/backendless/UserServiceAndroidExtra.java
@@ -1,3 +1,21 @@
+/*
+ * ********************************************************************************************************************
+ *  <p/>
+ *  BACKENDLESS.COM CONFIDENTIAL
+ *  <p/>
+ *  ********************************************************************************************************************
+ *  <p/>
+ *  Copyright 2012 BACKENDLESS.COM. All Rights Reserved.
+ *  <p/>
+ *  NOTICE: All information contained herein is, and remains the property of Backendless.com and its suppliers,
+ *  if any. The intellectual and technical concepts contained herein are proprietary to Backendless.com and its
+ *  suppliers and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret
+ *  or copyright law. Dissemination of this information or reproduction of this material is strictly forbidden
+ *  unless prior written permission is obtained from Backendless.com.
+ *  <p/>
+ *  ********************************************************************************************************************
+ */
+
 package com.backendless;
 
 import com.backendless.async.callback.AsyncCallback;
@@ -11,7 +29,10 @@ import com.facebook.SessionState;
 import org.json.JSONObject;
 
 import java.lang.reflect.Field;
-import java.util.*;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 class UserServiceAndroidExtra
 {
@@ -83,6 +104,13 @@ class UserServiceAndroidExtra
                          Map<String, String> twitterFieldsMappings, AsyncCallback<BackendlessUser> responder )
   {
     new AbstractSocialLoginStrategy.Builder( context, webView, AbstractSocialLoginStrategy.SocialType.TWITTER, twitterFieldsMappings, null, getSocialDialogResponder( responder ) ).build().run();
+  }
+
+  void loginWithGooglePlus( android.app.Activity context, android.webkit.WebView webView,
+                          Map<String, String> googlePlusFieldsMappings, List<String> permissions,
+                          final AsyncCallback<BackendlessUser> responder )
+  {
+    new AbstractSocialLoginStrategy.Builder( context, webView, AbstractSocialLoginStrategy.SocialType.GOOGLE_PLUS, googlePlusFieldsMappings, permissions, getSocialDialogResponder( responder ) ).build().run();
   }
 
   private AsyncCallback<JSONObject> getSocialDialogResponder( final AsyncCallback<BackendlessUser> responder )
