339caa793a1d23750ff087ba75ce4d3af913a14e
Anatolii Stepaniuk <anatoliy.stepanyuk@themidnightcoders.com>
2016-02-18 19:11:45 +0200

    BKNDLSS-11726 - removed unused methods

--- src/com/backendless/utils/JsonHelper.java
+++ src/com/backendless/utils/JsonHelper.java
@@ -11,33 +11,6 @@ import java.util.List;
 import java.util.Map;
 
 public class JsonHelper {
-    public static Object toJSON(Object object) throws JSONException {
-        if (object instanceof Map) {
-            JSONObject json = new JSONObject();
-            Map map = (Map) object;
-            for (Object key : map.keySet()) {
-                json.put(key.toString(), toJSON(map.get(key)));
-            }
-            return json;
-        } else if (object instanceof Iterable) {
-            JSONArray json = new JSONArray();
-            for (Object value : ((Iterable)object)) {
-                json.put(value);
-            }
-            return json;
-        } else {
-            return object;
-        }
-    }
-
-    public static boolean isEmptyObject(JSONObject object) {
-        return object.names() == null;
-    }
-
-    public static Map<String, Object> getMap(JSONObject object, String key) throws JSONException {
-        return toMap(object.getJSONObject(key));
-    }
-
     public static Map<String, Object> toMap(JSONObject object) throws JSONException {
         Map<String, Object> map = new HashMap();
         Iterator keys = object.keys();
