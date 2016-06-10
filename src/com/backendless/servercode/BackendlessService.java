ae4f375bbdd325e3559168e79b9e72c8d67bb674
Oleg Vyalyh <oleg.vyalyh@themidnightcoders.com>
2016-06-10 11:38:01 +0300

    service annotation

--- src/com/backendless/servercode/BackendlessService.java
+++ src/com/backendless/servercode/BackendlessService.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Target( ElementType.TYPE )
 @Retention( RetentionPolicy.RUNTIME )
-public @interface BackendlessServiceInfo
+public @interface BackendlessService
 {
   String name();
   String version() default "1.0.0";
