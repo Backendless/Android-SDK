931fd6973e0031d7d45f873944f748c4dfb843ed
Oleg Vyalyh <oleg.vyalyh@themidnightcoders.com>
2016-08-05 15:29:04 +0300

    HeadersManager

--- src/com/backendless/BLHeadersManager.java
+++ src/com/backendless/BLHeadersManager.java
@@ -7,38 +7,27 @@ import com.backendless.exceptions.ExceptionMessage;
 import java.util.Hashtable;
 import java.util.Map;
 
-
-public class BLHeadersManager implements IHeadersManager
+class BLHeadersManager implements IHeadersManager
 {
-  private final Object headersLock = new Object();
-  private Hashtable<String, String> headers = new Hashtable<String, String>();
+  private static BLHeadersManager instance = new BLHeadersManager();
 
-  private static ThreadLocal<BLHeadersManager> threadLocal = new InheritableThreadLocal<BLHeadersManager>()
+  private static ThreadLocal<BLHeadersManagerImpl> threadLocal = new InheritableThreadLocal<BLHeadersManagerImpl>()
   {
     @Override
-    protected BLHeadersManager initialValue()
+    protected BLHeadersManagerImpl initialValue()
     {
-      return new BLHeadersManager();
+      return new BLHeadersManagerImpl();
     }
   };
 
-  static BLHeadersManager getInstance()
-  {
-    return BLHeadersManager.threadLocal.get();
-  }
-
   private BLHeadersManager()
   {
-    if( Backendless.getApplicationId() == null || Backendless.getSecretKey() == null )
-    {
-      throw new IllegalStateException( ExceptionMessage.NOT_INITIALIZED );
-    }
 
-    addHeader( HeadersManager.HeadersEnum.APP_ID_NAME, Backendless.getApplicationId() );
-    addHeader( HeadersManager.HeadersEnum.SECRET_KEY_NAME, Backendless.getSecretKey() );
-    addHeader( HeadersManager.HeadersEnum.APP_TYPE_NAME, DeviceType.BL.name() );
-    addHeader( HeadersManager.HeadersEnum.API_VERSION, "1.0" );
-    addHeaders( Backendless.getHeaders() );
+  }
+
+  static BLHeadersManager getInstance()
+  {
+    return instance;
   }
 
   public void cleanHeaders()
@@ -46,65 +35,128 @@ public class BLHeadersManager implements IHeadersManager
     threadLocal.remove();
   }
 
+  @Override
   public void addHeader( HeadersManager.HeadersEnum headersEnum, String value )
   {
-    synchronized( headersLock )
-    {
-      headers.put( headersEnum.getHeader(), value );
-    }
+    threadLocal.get().addHeader( headersEnum, value );
   }
 
+  @Override
   public void addHeaders( Map<String, String> headers )
   {
-    if( headers == null || headers.isEmpty() )
-      return;
-
-    synchronized( headersLock )
-    {
-      this.headers.putAll( headers );
-    }
+    threadLocal.get().addHeaders( headers );
   }
 
+  @Override
   public void removeHeader( HeadersManager.HeadersEnum headersEnum )
   {
-    synchronized( headersLock )
-    {
-      headers.remove( headersEnum.getHeader() );
-    }
+    threadLocal.get().removeHeader( headersEnum );
   }
 
+  @Override
   public Hashtable<String, String> getHeaders() throws BackendlessException
   {
-    synchronized( headersLock )
-    {
-      if( headers == null || headers.isEmpty() )
-        throw new IllegalStateException( ExceptionMessage.NOT_INITIALIZED );
-
-      return headers;
-    }
+    return threadLocal.get().getHeaders();
   }
 
+  @Override
   public void setHeaders( Map<String, String> headers )
   {
-    synchronized( headersLock )
-    {
-      this.headers.putAll( headers );
-    }
+    threadLocal.get().setHeaders( headers );
   }
 
+  @Override
   public String getHeader( HeadersManager.HeadersEnum headersEnum ) throws BackendlessException
   {
-    synchronized( headersLock )
+    return threadLocal.get().getHeader( headersEnum );
+  }
+
+
+  static class BLHeadersManagerImpl implements IHeadersManager
+  {
+    private final Object headersLock = new Object();
+    private Hashtable<String, String> headers = new Hashtable<>();
+
+    private BLHeadersManagerImpl()
+    {
+      if( Backendless.getApplicationId() == null || Backendless.getSecretKey() == null )
+      {
+        throw new IllegalStateException( ExceptionMessage.NOT_INITIALIZED );
+      }
+      addHeader( HeadersManager.HeadersEnum.APP_ID_NAME, Backendless.getApplicationId() );
+      addHeader( HeadersManager.HeadersEnum.SECRET_KEY_NAME, Backendless.getSecretKey() );
+      addHeader( HeadersManager.HeadersEnum.APP_TYPE_NAME, DeviceType.BL.name() );
+      addHeader( HeadersManager.HeadersEnum.API_VERSION, "1.0" );
+      addHeaders( Backendless.getHeaders() );
+    }
+
+    public void cleanHeaders()
+    {
+      headers.clear();
+    }
+
+    public void addHeader( HeadersManager.HeadersEnum headersEnum, String value )
+    {
+      synchronized( headersLock )
+      {
+        headers.put( headersEnum.getHeader(), value );
+      }
+    }
+
+    public void addHeaders( Map<String, String> headers )
     {
       if( headers == null || headers.isEmpty() )
-        throw new IllegalStateException( ExceptionMessage.NOT_INITIALIZED );
+        return;
 
-      String header = headers.get( headersEnum );
+      synchronized( headersLock )
+      {
+        this.headers.putAll( headers );
+      }
+    }
 
-      if( header == null )
-        header = headers.get( headersEnum.getHeader() );
+    public void removeHeader( HeadersManager.HeadersEnum headersEnum )
+    {
+      synchronized( headersLock )
+      {
+        headers.remove( headersEnum.getHeader() );
+      }
+    }
 
-      return header;
+    public Hashtable<String, String> getHeaders() throws BackendlessException
+    {
+      synchronized( headersLock )
+      {
+        if( headers == null || headers.isEmpty() )
+          throw new IllegalStateException( ExceptionMessage.NOT_INITIALIZED );
+
+        return headers;
+      }
+    }
+
+    public void setHeaders( Map<String, String> headers )
+    {
+      synchronized( headersLock )
+      {
+        this.headers.putAll( headers );
+      }
+    }
+
+    public String getHeader( HeadersManager.HeadersEnum headersEnum ) throws BackendlessException
+    {
+      synchronized( headersLock )
+      {
+        if( headers == null || headers.isEmpty() )
+          throw new IllegalStateException( ExceptionMessage.NOT_INITIALIZED );
+
+        String header = headers.get( headersEnum );
+
+        if( header == null )
+          header = headers.get( headersEnum.getHeader() );
+
+        return header;
+      }
     }
   }
 }
+
+
