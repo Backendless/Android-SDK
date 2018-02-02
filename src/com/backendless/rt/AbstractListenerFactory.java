ece023fd957b03c42a3b4fe5f9d87f739e077862
sergey.kukurudzyak <sergey@backendless.com>
2018-02-02 10:32:58 +0200

    fixed BKNDLSS-16554 [ANDROID][RT]Each of listener should be new instance

--- src/com/backendless/rt/AbstractListenerFactory.java
+++ src/com/backendless/rt/AbstractListenerFactory.java
@@ -1,31 +1,10 @@
 package com.backendless.rt;
 
-import java.util.concurrent.ConcurrentHashMap;
-
 public abstract class AbstractListenerFactory<E extends RTListener>
 {
-  private final ConcurrentHashMap<String, E> listeners = new ConcurrentHashMap<>();
-  private final Object lock = new Object();
-
   protected E create( final String key, Provider<E> provider )
   {
-    E listener = listeners.get( key );
-
-    if( listener == null )
-    {
-      synchronized( lock )
-      {
-        listener = listeners.get( key );
-
-        if( listener == null )
-        {
-          listener = provider.create();
-          listeners.put( key, listener );
-        }
-      }
-    }
-
-    return listener;
+    return provider.create();
   }
 
   protected interface Provider<T extends RTListener>
