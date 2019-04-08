cd3f23b88975ac2fdceb82458e201637a35bfd20
sergey.kukurudzyak <sergey@backendless.com>
2019-04-08 15:58:13 +0300

    removed fake developer changed health check for web console

--- scripts/mounts/mysql/init/3_CreateFakeDeveloper.sql
+++
@@ -1,6 +0,0 @@
-USE `main_backendless`;
-
-INSERT INTO `Developer` (`id`,`name`,`email`,`password`,`developerStatusId`,`lastLogin`,`registrationDate`,`companyName`,`phone`) VALUES ('1','Bob','foo@foo.com','$2a$10$o9uAWCeKSCWbj91rsxeR0u345oy.HHN1vi8fuV7kQYb5y3vPbeOIe','1',NULL,NULL,NULL,NULL);
-INSERT INTO `Developer` (`id`,`name`,`email`,`password`,`developerStatusId`,`lastLogin`,`registrationDate`,`companyName`,`phone`) VALUES ('2','Jack','bar@bar','secret','1',NULL,NULL,NULL,NULL);
-INSERT INTO `Developer` (`id`,`name`,`email`,`password`,`developerStatusId`,`lastLogin`,`registrationDate`,`companyName`,`phone`) VALUES ('3','John','foo@bar','secret','1',NULL,NULL,NULL,NULL);
-INSERT INTO `Developer` (`id`,`name`,`email`,`password`,`developerStatusId`,`lastLogin`,`registrationDate`,`companyName`,`phone`) VALUES ('4','oleg','oleg@themidnightcoders.com','$2a$10$o9uAWCeKSCWbj91rsxeR0u345oy.HHN1vi8fuV7kQYb5y3vPbeOIe','1',NULL,NULL,NULL,NULL);
