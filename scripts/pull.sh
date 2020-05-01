4c351ad4a10d697bb2f30d21608abac993abec05
sergey.kukurudzyak <sergey@backendless.com>
2020-05-01 11:42:52 +0300

    split pull script to pull and pull_cloud

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -8,44 +8,28 @@ echo "Usage: \"`basename "$0"` <container_version>\""
 cd `dirname "$0"`;
 
 VERSION=${1:?"You should provide artifact version."}
-REGISTRY=${2:?"You should provide registry."}
-
 
 # bl-server
-docker pull $REGISTRY/bl-server:$VERSION
+docker pull backendless/bl-server:$VERSION
 
 # bl-upgrade
-docker pull $REGISTRY/bl-upgrade:$VERSION
+docker pull backendless/bl-upgrade:$VERSION
 
 # bl-coderunner-java
-docker pull $REGISTRY/bl-coderunner-java:$VERSION
+docker pull backendless/bl-coderunner-java:$VERSION
 
 # bl-coderunner-js
-docker pull $REGISTRY/bl-coderunner-js:$VERSION
+docker pull backendless/bl-coderunner-js:$VERSION
 
 # bl-web-console
-docker pull $REGISTRY/bl-web-console:$VERSION
+docker pull backendless/bl-web-console:$VERSION
 
 # bl-config-loader
-docker pull $REGISTRY/bl-config-loader:$VERSION
+docker pull backendless/bl-config-loader:$VERSION
 
 # bl-hazelcast
-docker pull $REGISTRY/bl-hazelcast:$VERSION
+docker pull backendless/bl-hazelcast:$VERSION
 
 # bl-rt-server
-docker pull $REGISTRY/bl-rt-server:$VERSION
-
-if [[ "$REGISTRY" == "registry.backendless.com:5000"  ]]; then
-	# bl-coderunner-js-cloud
-	docker pull $REGISTRY/bl-coderunner-js-cloud:$VERSION
-
-	# bl-billing
-	docker pull $REGISTRY/bl-billing:$VERSION
-
-	# bl-limit
-	docker pull $REGISTRY/bl-limit:$VERSION
-
-	# bl-game-server
-	docker pull $REGISTRY/bl-game-server:$VERSION
-fi
+docker pull backendless/bl-rt-server:$VERSION
 
