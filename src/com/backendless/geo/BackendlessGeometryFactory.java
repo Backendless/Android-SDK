32425c3dcfed3513b1c34b4132fdd6684ae0a7ce
Vladimir Yalovy <vladimir@themidnightcoders.com>
2020-09-01 14:41:13 +0300

    [BKNDLSS-22451]: BackendlessGeometryFactory can adapt GeoJSON

--- src/com/backendless/geo/BackendlessGeometryFactory.java
+++ src/com/backendless/geo/BackendlessGeometryFactory.java
@@ -2,6 +2,7 @@ package com.backendless.geo;
 
 import com.backendless.persistence.Geometry;
 import com.backendless.persistence.GeometryDTO;
+import com.backendless.persistence.SpatialReferenceSystemEnum;
 import weborb.reader.AnonymousObject;
 import weborb.reader.NamedObject;
 import weborb.reader.NullType;
@@ -10,6 +11,7 @@ import weborb.types.IAdaptingType;
 import weborb.util.IArgumentObjectFactory;
 
 import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.Map;
 
 
@@ -36,20 +38,77 @@ public class BackendlessGeometryFactory implements IArgumentObjectFactory
       Map<String, Object> properties = (Map<String, Object>) adaptingType.defaultAdapt();
       String geoJson = (String) properties.get( "geoJson" );
 
-      if (geoJson == null)
-        return null;
+      // If GeometryDTO
+      if (geoJson != null)
+      {
+        String geomClass = (String) properties.get( "geomClass" );
+        Integer srsId = (Integer) properties.get( "srsId" );
 
-      String geomClass = (String) properties.get( "geomClass" );
-      Integer srsId = (Integer) properties.get( "srsId" );
+        Geometry geometry = new GeometryDTO( geomClass, srsId, geoJson ).toGeometry();
+        refCache.addObject( adaptingType, GeometryDTO.class, geometry );
+        return geometry;
+      }
 
-      Geometry geometry = new GeometryDTO( geomClass, srsId, geoJson ).toGeometry();
-      refCache.addObject( adaptingType, GeometryDTO.class, geometry );
-      return geometry;
+      Object[] coordinates = (Object[]) properties.get( "coordinates" );
+      // If GeoJSON
+      if( coordinates != null )
+      {
+        Integer srsId = (Integer) properties.get( "srsId" );
+        SpatialReferenceSystemEnum srs = SpatialReferenceSystemEnum.valueBySRSId( srsId );
+        String type = (String) properties.get( "type" );
+
+        String geoJsonBuilder = "{\"type\":\"" + type + "\",\"coordinates\":" + jsonCoordinatePairs( type, coordinates ) + "}";
+        Geometry geometry = Geometry.fromGeoJSON( geoJsonBuilder, srs );
+
+        refCache.addObject( adaptingType, GeometryDTO.class, geometry );
+        return geometry;
+      }
+
+      return null;
     }
     else
     {
       throw new RuntimeException( "Can not create BackendlessGeometry from type " + adaptingType.getClass().getName() );
     }
+  }
+
+  private String jsonCoordinatePairs( String type, Object[] coordinates )
+  {
+    switch( type )
+    {
+      case "Point":
+        return Arrays.toString( coordinates );
+      case "LineString":
+        return createLineStringCoordinatePairs( coordinates );
+      case "Polygon":
+        return createPolygonCoordinatePairs( coordinates );
+      default:
+        throw new RuntimeException( "Can not create BackendlessGeometry, wrong Geometry type");
+    }
+  }
+
+  private String createPolygonCoordinatePairs( Object[] coordinates )
+  {
+    StringBuilder sb = new StringBuilder();
+    sb.append( '[' );
+
+    for( Object object : coordinates )
+      sb.append( createLineStringCoordinatePairs( (Object[]) object ) ).append( "," );
+
+    sb.setCharAt( sb.length() - 1, ']' );
+    return sb.toString();
+  }
+
+  private String createLineStringCoordinatePairs( Object[] coordinates )
+  {
+    StringBuilder sb = new StringBuilder();
+    sb.append( '[' );
+
+    for( Object object : coordinates )
+      sb.append( Arrays.toString( (Object[]) object ) ).append( "," );
+
+    sb.setCharAt( sb.length() - 1, ']' );
+    return sb.toString();
   }
 
   @Override
