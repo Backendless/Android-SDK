c26eac4fe3ec375cc7f5c7dfa6f9089627160f64
andriy.konoz <andriy.konoz@themidnightcoders.com>
2020-09-29 14:48:20 +0300

    [BKNDLSS-22852] ISE for BL with 'createCSV' event handlers

--- scripts/mounts/mysql/init/2_StaticInsertValuesMainBackendless.sql
+++ scripts/mounts/mysql/init/2_StaticInsertValuesMainBackendless.sql
@@ -1,6 +1,6 @@
 USE `main_backendless`;
 
-INSERT INTO `Version` (`main`, `application`) values (33, 77);
+INSERT INTO `Version` (`main`, `application`) values (33, 78);
 
 INSERT INTO `DeveloperStatus` (`id`, `name`) VALUES ('1', 'ACTIVE');
 INSERT INTO `DeveloperStatus` (`id`, `name`) VALUES ('2', 'SUSPENDED');
